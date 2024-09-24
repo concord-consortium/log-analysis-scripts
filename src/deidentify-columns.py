c0e4f5b661e78ff0883788dd116f6e143449760e
Boris Goldowsky <bgoldowsky@concord.org>
2024-09-24 15:55:46 -0400

    FIx looping structure & command-line args

--- src/deidentify-columns.py
+++ src/deidentify-columns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import shortuuid
-from tqdm import tqdm
 import argparse
 import csv
 import sys
@@ -9,10 +8,10 @@ import sys
 parser = argparse.ArgumentParser(description="De-identify a list of columns from a CSV file.",
                                  epilog="The columns are masked using UUIDs and a separate mapping file is written to map.csv")
 parser.add_argument("filename", help="CSV file")
-parser.add_argument("-f", "--fields", default="[student_name, school]", help="Array of fields to de-identify")
+parser.add_argument("-c", "--column", action="append", help="Heading of a column to de-identify. Can be specified more than once.")
 parser.add_argument("-v", "--verbose", action="store_true", help="Print information while running")
 
-def deidentify_fields(filename, fields):
+def deidentify_fields(filename, columns):
     with open(filename, "r") as file:
         csv_reader = csv.reader(file)
         writer = csv.writer(sys.stdout)
@@ -20,31 +19,32 @@ def deidentify_fields(filename, fields):
         # Get the header
         header = next(csv_reader)
 
-        for field in tqdm(fields, desc="De-identifying fields..."):
+        col_indexes = []
+
+        for col in columns:
             try:
-                field_index = header.index(field)
+                col_indexes.append(header.index(col))
             except ValueError:
-                sys.stderr.write("Error: Could not find " + field + " column; columns are: " + ", ".join(header))
-                exit(1) 
+                sys.stderr.write("Error: Could not find " + col + " column; columns are: " + ", ".join(header))
+                exit(1)
             if (args.verbose):
-                print('De-identifying column: ' + field)
-            rows = 0
-            for row in csv_reader:
-                rows += 1
-                if (args.verbose and rows % 1000 == 0):
-                    sys.stderr.write(f"Processed {rows} rows\n")
-                
-                data = row[field_index]
-                row.pop(field_index)
-                
+                sys.stderr.write('De-identifying column: ' + col + '\n')
+
+        rows = 0
+        for row in csv_reader:
+            rows += 1
+            if (args.verbose and rows % 1000 == 0):
+                sys.stderr.write(f"Processed {rows} rows\n")
+            
+            for col_index in col_indexes:
+                data = row[col_index]
+            
                 if (data):
                     mask = shortuuid.uuid(name=data)
-                    row.append(mask)
-                else:
-                    row.append(None)
-                writer.writerow(row)
+                    row[col_index] = mask
+
+            writer.writerow(row)
 
 if __name__ == "__main__":
-    print('Hit main')
     args = parser.parse_args()
-    deidentify_fields(args.filename, args.fields)
+    deidentify_fields(args.filename, args.column)
